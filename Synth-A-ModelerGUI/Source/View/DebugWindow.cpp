0bd6befd3fa34e46a4b7768fd993b5f2e7f1df60
Peter Vasil <mail@petervasil.net>
2012-05-12 16:29:13 +0200

    Add workaround for storing correct debugWindow position in settings.

--- Synth-A-ModelerGUI/Source/View/DebugWindow.cpp
+++ Synth-A-ModelerGUI/Source/View/DebugWindow.cpp
@@ -46,7 +46,14 @@ DocumentWindow("Output", Colours::darkgrey, DocumentWindow::closeButton)
 
 DebugWindow::~DebugWindow()
 {
-    // save the current size and position to our settings file..
+	// Workaround to prevent wrong bound values to be stored in user settings on linux
+#if JUCE_LINUX
+	Rectangle<int> br = getBounds();
+	br.setHeight(getHeight()-28);
+	br.setWidth(getWidth()-2);
+	setBounds(br);
+#endif
+
     StoredSettings::getInstance()->getProps()
         .setValue ("lastDebugWindowPos", getWindowStateAsString());
 
