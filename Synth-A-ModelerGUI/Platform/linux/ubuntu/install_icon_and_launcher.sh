646c243a28f9df13545bdca6eb257fdd5ae93367
Peter Vasil <mail@petervasil.net>
2012-05-14 12:12:55 +0200

    Add installer script and launcher for ubuntu.

---
+++ Synth-A-ModelerGUI/Platform/linux/ubuntu/install_icon_and_launcher.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+
+ICON_PATH=$HOME/.local/share/icons
+LAUNCHER_PATH=$HOME/.local/share/applications
+BIN_PATH=$HOME/bin
+
+echo "\nSynth-A-ModelerGUI installer :)\n"
+echo "Install executable..."
+mkdir -p $BIN_PATH
+cp $BASEDIR/../../../Builds/Linux/build/Synth-A-ModelerGUI $BIN_PATH
+
+echo "Install launcher..."
+mkdir -p $LAUNCHER_PATH
+cp $BASEDIR/synthamodeler.desktop $LAUNCHER_PATH
+
+echo "Install icon..."
+mkdir -p $ICON_PATH
+cp $BASEDIR/../../../BinaryData/synthamodeler_icon.png.desktop $ICON_PATH
+
+echo "Done."
