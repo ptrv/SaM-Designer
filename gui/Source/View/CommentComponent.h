df1bb5c288821c8c9ffd387f29e367b68b009cca
Peter Vasil <mail@petervasil.net>
2012-10-17 23:11:13 +0200

    Start implementing comment component.

---
+++ gui/Source/View/CommentComponent.h
@@ -0,0 +1,46 @@
+/*
+  ==============================================================================
+
+    CommentComponent.h
+    Created: 17 Oct 2012 4:56:15pm
+    Author:  Peter Vasil
+
+  ==============================================================================
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+*/
+
+#ifndef __COMMENTCOMPONENT_H_FE5F7A49__
+#define __COMMENTCOMPONENT_H_FE5F7A49__
+
+class BaseObjectComponent;
+class ObjController;
+
+class CommentComponent : public BaseObjectComponent,
+                         public ChangeListener,
+                         public ChangeBroadcaster,
+                         public SelectableObject
+{
+public:
+    CommentComponent(ObjController& owner_, ValueTree data_);
+    ~CommentComponent();
+private:
+
+};
+
+
+
+#endif  // __COMMENTCOMPONENT_H_FE5F7A49__
