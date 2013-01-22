cf64e6b80e8f42d48d7c180f11004f2d0569f00e
Peter Vasil <mail@petervasil.net>
2013-01-22 22:02:53 +0100

    Add Mac support for re2 build script.
    +Remove precompiled libre2.a
    +Make build script work also for Mac
    +Change lin include path for xcode project

---
+++ gui/Libs/get_and_compile_re2.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+
+os=${OSTYPE//[0-9.]/}
+echo "OS Type: $os"
+if [[ $os == "darwin" ]]; then
+    script=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $0`
+    scriptpath=`dirname $script`
+else
+    script=`readlink -f $0`
+    scriptpath=`dirname $script`
+fi
+
+re2_url="https://re2.googlecode.com/files/re2-20121127.tgz"
+re2_archive="re2-20121127.tgz"
+
+# download re2
+cd $scriptpath
+if [[ $os == "darwin" ]]; then
+    curl -L $re2_url -o $re2_archive
+else
+    wget $re2_url -O $re2_archive
+fi
+
+tar -xvzf $re2_archive
+rm $re2_archive
+
+# compile
+cd re2
+if [[ $os == "darwin" ]]; then
+    num_jobs=`/usr/sbin/system_profiler -detailLevel full SPHardwareDataType | awk '/Total Number [Oo]f Cores/ {print $5};'`
+    make -e CXXFLAGS="-Wall -O3 -g -pthread -arch i386" -e LDFLAGS="-pthread -arch i386" -j$num_jobs
+else
+    num_jobs=`grep -c 'model name' /proc/cpuinfo`
+    make -j$num_jobs
+fi
