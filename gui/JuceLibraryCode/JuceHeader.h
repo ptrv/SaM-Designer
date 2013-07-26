9570915d8938a395a6683884d5d5635de37b5ca9
Peter Vasil <mail@petervasil.net>
2013-07-26 13:50:00 +0200

    Version 0.2.0

--- gui/JuceLibraryCode/JuceHeader.h
+++ gui/JuceLibraryCode/JuceHeader.h
@@ -32,8 +32,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "Synth-A-Modeler-Designer";
-    const char* const  versionString  = "0.1.0";
-    const int          versionNumber  = 0x100;
+    const char* const  versionString  = "0.2.0";
+    const int          versionNumber  = 0x200;
 }
 
 #endif   // __APPHEADERFILE_VKFX9M__
